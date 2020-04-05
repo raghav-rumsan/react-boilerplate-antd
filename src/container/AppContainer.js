b64d9debd57b730bff04093bfaf15d891eea41c2
Raghav <arg.ahv7@gmail.com>
2020-04-05 12:56:04 +0545

    changes

--- src/container/AppContainer.js
+++ src/container/AppContainer.js
@@ -4,23 +4,18 @@ import { Router } from "@reach/router";
 import { createStructuredSelector } from "reselect";
 import { getClient, getUser } from "./actions";
 import { connect } from "react-redux";
-import {
-  PublicRoute,
-  PageNotFound,
-  ProtectedRoute,
-  GuestRoute
-} from "./components";
+import { PublicRoute, PageNotFound, GuestRoute } from "./components";
 import { Login } from "./guest";
 import LayoutContainer from "../components/layout/LayoutContainer";
 import { selectToken } from "./selectors";
 // import "antd/dist/antd.dark.css";
+// import "antd/dist/antd.css";
 
 const AppContainer = ({ token, getUser, theme }) => {
   // useEffect(() => {
   //   getClient();
   // }, []);
   // const [theme] = useState(null);
-  console.log("localstorage.getItem(theme", localStorage.getItem("theme"));
 
   useEffect(() => {
     api.defaults.headers.common.Authorization = token;
@@ -34,6 +29,7 @@ const AppContainer = ({ token, getUser, theme }) => {
       <LayoutContainer>
         <Router>
           {/* Routes goes here */}
+          <GuestRoute container={Login} path="/login" />
           <PublicRoute container={PageNotFound} path="*" />
         </Router>
       </LayoutContainer>
@@ -42,12 +38,12 @@ const AppContainer = ({ token, getUser, theme }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-  token: selectToken
+  token: selectToken,
 });
 
 const mapDispatchToProps = {
   getUser,
-  getClient
+  getClient,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(AppContainer);
